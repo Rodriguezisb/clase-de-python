7029650fdd2d64b84333611d94e513aa35b1a9f4
Isbelys <rodriguezjisbelys@gmail.com>
2024-05-11 18:59:25 -0300

    condicionales1

---
+++ condicionales1/if.py
@@ -0,0 +1,10 @@
+
+edad1= int(input("ingresa a edad de la primera persona"))
+
+edad2= int(input("ingresa a edad de la segunda persona"))
+
+if edad1<edad2:
+
+    print("La persona 1 es menor que la persona 2")
+
+    
---
+++ condicionales1/oplogicos.py
@@ -0,0 +1,11 @@
+
+
+
+edad= int(inptu("Dime tu edad: "))
+hora= int(input("die la hora: "))
+
+if edad<18 and hora<8 :
+
+    print("eres menor de edad")
+
+    
